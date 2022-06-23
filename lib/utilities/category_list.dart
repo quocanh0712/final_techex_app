3fdfef303c864c0ed3b1a4af8a8c974d8456dabc
Quốc anh Trương <quocanhtruong@MacBook-Pro-cua-Quoc.local>
2022-06-23 09:42:09 +0700

    feature: bottom navbar

---
+++ lib/utilities/category_list.dart
@@ -0,0 +1,69 @@
+List<String> maincategory = [
+  'phone & accessories',
+  'electronic device',
+  'computer & laptop',
+  'camera & camcorder',
+  'smart watch',
+  'household appliances',
+];
+
+List<String> phoneandaccessories = [
+  'phone',
+  'tablet',
+  'backup charger',
+  'battery and charger',
+  'phone case , sticker',
+  'screen protector',
+  'memory card',
+  'sim',
+  'landline',
+  'other',
+];
+
+List<String> electronicdevice = [
+  'game console',
+  'console accessories'
+      'loudspeaker',
+  'earphone',
+  'headphone',
+  'television',
+  'game disc',
+  'television accessories'
+];
+
+List<String> computerandlaptop = [
+  'computer',
+  'laptop',
+  'screen',
+  'storage device',
+  'computer components',
+  'network equipment',
+  'printer, scanner, projector',
+  'Computer accessories',
+  'other',
+];
+
+List<String> cameraandcamcorder = [
+  'camera',
+  'camcorder',
+  'surveillance cameras and system cameras',
+  'cam memory card',
+  'lens',
+  'camera accessories',
+  'flycam',
+];
+
+List<String> smartwatch = [
+  'smart watch',
+  'smart watch accessories',
+  'smart watch screen protector'
+];
+
+List<String> householdappliances = [
+  'kitchen utensils',
+  'vacuum cleaners and cleaning equipment',
+  'fan and air conditioner',
+  'electric stove',
+  'clothing care equipment',
+  'other',
+];
