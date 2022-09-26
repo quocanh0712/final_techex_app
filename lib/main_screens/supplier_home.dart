28bde2aef0c7b6511ecff1df34d2cb1be74108da
quocanh0712 <hideonbush071299@gmail.com>
2022-09-26 23:52:29 +0700

    added countup animation and finished to design balance screen

--- lib/main_screens/supplier_home.dart
+++ lib/main_screens/supplier_home.dart
@@ -1,7 +1,10 @@
+import 'package:badges/badges.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_techex_app/main_screens/category.dart';
 import 'package:final_techex_app/main_screens/dashboard.dart';
 import 'package:final_techex_app/main_screens/stores.dart';
 import 'package:final_techex_app/main_screens/upload_product.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'home.dart';
@@ -24,44 +27,69 @@ class _SupplierHomeScreenState extends State<SupplierHomeScreen> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _tabs[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 0,
-        type: BottomNavigationBarType.fixed,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w700),
-        backgroundColor: const Color.fromARGB(255, 31, 129, 117),
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.white,
-        currentIndex: _selectedIndex,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Category',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shop),
-            label: 'Stores',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard),
-            label: 'Dashboard',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.upload),
-            label: 'Upload',
-          ),
-        ],
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-      ),
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('orders')
+            .where('sid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .where('deliverystatus', isEqualTo: 'preparing')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Material(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          return Scaffold(
+            body: _tabs[_selectedIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              elevation: 0,
+              type: BottomNavigationBarType.fixed,
+              selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w700),
+              backgroundColor: const Color.fromARGB(255, 31, 129, 117),
+              selectedItemColor: Colors.black,
+              unselectedItemColor: Colors.white,
+              currentIndex: _selectedIndex,
+              items: [
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: 'Category',
+                ),
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.shop),
+                  label: 'Stores',
+                ),
+                BottomNavigationBarItem(
+                  icon: Badge(
+                      showBadge: snapshot.data!.docs.isEmpty ? false : true,
+                      padding: const EdgeInsets.all(2),
+                      badgeColor: const Color.fromARGB(255, 77, 230, 82),
+                      badgeContent: Text(
+                        snapshot.data!.docs.length.toString(),
+                        style:
+                            const TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                      child: const Icon(Icons.dashboard)),
+                  label: 'Dashboard',
+                ),
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.upload),
+                  label: 'Upload',
+                ),
+              ],
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          );
+        });
   }
 }
